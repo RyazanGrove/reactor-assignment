e80d74066e808c7958ac7fd0357c834c30ce04ec
Mikhail R <RyazanGrove@users.noreply.github.com>
2021-02-23 14:51:50 +0200

    simple styles for product have been added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './product.css'
 
 const Product = (props) => {
     return(
-        <div>
-            {props.productInfo.id}{props.productInfo.manufacturer}{props.productInfo.name}{props.productInfo.price}{props.productInfo.type}{props.productInfo.color}{props.availability}
+        <div className="productFrame">
+            <div className="productName">{props.productInfo.name}</div>
+            <div className="productManufacturer">manufacturer: {props.productInfo.manufacturer}</div>
+            <div className="priceAndTypeContainer">
+                <div className="productPrice">
+                    Price: <br/><div className="price">{props.productInfo.price}</div>
+                </div>
+                <div className="productType">
+                    Type: <br/>{props.productInfo.type}
+                </div>
+            </div>
+            <div className="colorContainer">
+                Colors: {props.productInfo.color.map(color => <div className="colorItem" style={{'background-color': `${color}`}}></div>)}
+            </div>
+
+            {props.availability}
+
         </div>
     )
 }
